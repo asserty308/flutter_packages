c1e16cc4dd08887f968321d72b44be88f8866ef8
asserty308 <yannickassert@gmail.com>
2020-08-24 07:53:18 +0200

    updted tests

--- flutter_web/test/string_url_extension_test.dart
+++ flutter_web/test/string_url_extension_test.dart
@@ -4,20 +4,20 @@ import 'package:flutter_web/utility/string_url_extension.dart';
 void main() {
   group('removeUrlScheme', () {
     test('Remove http scheme', () {
-      final String result = 'http://www.intersolute.de'.removeUrlScheme();
-      expect(result, 'www.intersolute.de');
+      final String result = 'http://www.google.com'.removeUrlScheme();
+      expect(result, 'www.google.com');
     });
 
     test('Remove https scheme', () {
-      final String result = 'https://www.intersolute.de'.removeUrlScheme();
-      expect(result, 'www.intersolute.de');
+      final String result = 'https://www.google.com'.removeUrlScheme();
+      expect(result, 'www.google.com');
     });
   });
 
   group('toUrl', () {
-    test('intersolute.de should become https://intersolute.de/admin', () {
-      final String result = 'intersolute.de'.toUrl('admin');
-      expect(result, 'https://intersolute.de/admin');
+    test('google.de should become https://google.com/maps', () {
+      final String result = 'google.de'.toUrl('maps');
+      expect(result, 'https://google.de/maps');
     });
   });
   
